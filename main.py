0573e1985c79575dfef56a9964f43096856225bb
Francesca F <fodfran@gmail.com>
2019-09-15 09:42:00 -0400

    finished setting up login and register

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+import re
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -10,6 +11,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:SHINee5252008@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = '5252008'
 
 class Task(db.Model):
 
@@ -20,6 +22,73 @@ class Task(db.Model):
     def __init__(self, name):
         self.name = name
         self.completed = False
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password    
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect("/login")
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user_email = request.form['email']
+        user_password = request.form['password']
+        user = User.query.filter_by(email=user_email).first()
+        
+        if user and user.password == user_password:
+            session['email'] = user_email
+            flash("Logged in")
+            return redirect("/") 
+        else:
+            flash("User password incorrect or user does not exist", "error")
+            
+
+    return render_template("login.html")
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        user_email = request.form['email']
+        user_password = request.form['password']
+        user_verify = request.form['verify']
+
+        if not (re.match(r"^.{3,20}$", user_email) and re.match(r"^\w+@\w+\.\w+$", user_email)):
+            flash("That's not a valid email", "error")
+        if not re.match(r"^\w{3,20}$", user_password):
+            flash("That's not a valid password", "error")
+        if user_password != user_verify:
+            flash("Passwords don't match", "error")
+        if re.match(r"^.{3,20}$", user_email) and re.match(r"^\w+@\w+\.\w+$", user_email) and re.match(r"^\w{3,20}$", user_password) and user_password == user_verify:
+            existing_user = User.query.filter_by(email=user_email).first()
+            if not existing_user:
+                new_user = User(user_email, user_password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['email'] = user_email
+                flash("Registration complete!")
+                return redirect("/")
+            else:
+                flash("This user already exists", "error")
+
+    return render_template("register.html")
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
